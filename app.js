98c58bb38161445df82dba1e9e185e0085864c42
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2019-01-05 22:03:54 +0000

    version 8

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var Person = function(name) {
 
 util.inherits(Person, events.EventEmitter);
 
-var joe = new Person("Joe");
+var joe = new Person("Josh");
 var mike = new Person("Mike");
 var ryu = new Person("Ryu");
 var ken = new Person("Ken");
