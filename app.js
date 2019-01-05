173d0e600170138b13b3277bc0db18b0b5f247c3
Joshua Milcette <joshuamilcette.student@careerdevs.com>
2019-01-05 21:58:55 +0000

    version 6

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-var fs = require("fs");
+var counter = require("./count")
 
-
-fs.readFile("readme.txt", "utf8", function(err, data){
-    console.log("test1");
-    if(err) {
-        console.log("error!");
-    } else {
-        console.log("test2");
-        fs.writeFile("writeme.txt", data);
-    }
-});
-
-// fs.writeFileSync("writeme.txt", readMe);
-console.log("test3");
+console.log(counter(["Josh.", "Josh!", "Josh?"]));
